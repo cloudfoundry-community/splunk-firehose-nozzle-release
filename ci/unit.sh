08376871753c69fe57e9e05f61549edcd7eb2975
Matt Cholick <cholick@gmail.com>
2016-10-03 08:14:18 -0700

    Iterating on CI

--- ci/unit.sh
+++ ci/unit.sh
@@ -6,7 +6,7 @@ echo "Jumping through hoops for Go"
 export GOPATH=`pwd`
 export PATH=$GOPATH/bin:$PATH
 
-cp -r nozzle-release-repo/src/splunk-firehose-nozzle splunk-firehose-nozzle
+cp -r release-repo/src/splunk-firehose-nozzle splunk-firehose-nozzle
 mkdir -p src/github.com/cf-platform-eng
 mv splunk-firehose-nozzle src/github.com/cf-platform-eng
 
