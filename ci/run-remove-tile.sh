d5b9f2a8bfbc59bdb6a95f1e4221d776be3ea0c8
Matt Cholick <cholick@gmail.com>
2016-10-03 07:59:03 -0700

    Iterating on CI

---
+++ ci/run-remove-tile.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+
+TILE_GEN_DIR="$( cd "$1" && pwd )"
+POOL_DIR="$( cd "$2" && pwd )"
+PRODUCT="splunk"
+
+BIN_DIR="$( cd "${TILE_GEN_DIR}/bin" && pwd )"
+
+PCF="${BIN_DIR}/pcf"
+
+cd "${POOL_DIR}"
+
+echo "Available products:"
+$PCF products
+echo
+
+if ! $PCF is-installed "${PRODUCT}" ; then
+	echo "${PRODUCT} not installed - skipping removal"
+	exit 0
+fi
+
+echo "Uninstalling ${PRODUCT}"
+$PCF uninstall "${PRODUCT}"
+echo
+
+echo "Applying Changes"
+$PCF apply-changes
+echo
+
+echo "Available products:"
+$PCF products
+echo
+
+if $PCF is-installed "${PRODUCT}" ; then
+	echo "${PRODUCT} remains installed - remove failed"
+	exit 1
+fi
