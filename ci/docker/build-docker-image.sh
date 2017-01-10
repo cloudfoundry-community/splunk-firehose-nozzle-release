838bd41e481d9d0066d6e174395fa00dbc24541e
Matt Cholick <cholick@gmail.com>
2017-01-10 09:27:31 -0800

    Fix docker image build script path check.

--- ci/docker/build-docker-image.sh
+++ ci/docker/build-docker-image.sh
@@ -2,10 +2,10 @@
 
 echo "Building and publishing docker image"
 
-if [ "$0" != "./scripts/docker.sh" ]; then
+if [ "$0" != "./ci/docker/build-docker-image.sh" ]; then
     echo "'docker.sh' should be run from repository root"
     exit 1
 fi
 
-docker build ci -t cfplatformeng/splunk-ci --no-cache
+docker build ci/docker -t cfplatformeng/splunk-ci --no-cache
 docker push cfplatformeng/splunk-ci
