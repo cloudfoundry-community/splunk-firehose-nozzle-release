b3106a8f750f988df38638e5a9467c5c65e8a2aa
Shubham Jain <luckyj5@users.noreply.github.com>
2017-06-29 10:58:13 -0400

    fixed unit.sh script

--- ci/unit.sh
+++ ci/unit.sh
@@ -6,9 +6,9 @@ echo "Jumping through hoops for Go"
 export GOPATH=`pwd`
 export PATH=$GOPATH/bin:$PATH
 
-cp -r source-repo/src/splunk-firehose-nozzle splunk-firehose-nozzle
-mkdir -p src/github.com/cloudfoundry-community
-mv splunk-firehose-nozzle src/github.com/cloudfoundry-community
+mkdir -p src/github.com/cloudfoundry-community/splunk-firehose-nozzle
+cp -r source-repo/* src/github.com/cloudfoundry-community/splunk-firehose-nozzle
+
 
 cd src/github.com/cloudfoundry-community/splunk-firehose-nozzle
 
