f955dae76963450cef0a06e98e6fe3a2d70da230
Shubham Jain <luckyj5@users.noreply.github.com>
2017-06-29 10:24:59 -0400

    adding source-repo pipeline for CI

--- ci/unit.sh
+++ ci/unit.sh
@@ -6,7 +6,7 @@ echo "Jumping through hoops for Go"
 export GOPATH=`pwd`
 export PATH=$GOPATH/bin:$PATH
 
-cp -r release-repo/src/splunk-firehose-nozzle splunk-firehose-nozzle
+cp -r source-repo/src/splunk-firehose-nozzle splunk-firehose-nozzle
 mkdir -p src/github.com/cloudfoundry-community
 mv splunk-firehose-nozzle src/github.com/cloudfoundry-community
 
